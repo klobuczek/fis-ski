a06de3e7fb79deb2a87d48550f09fce15a670121
Heinrich Klobuczek <heinrich@mail.com>
2010-03-22 17:15:52 +0100

    order races by date

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,5 +1,5 @@
 class RacesController < ApplicationController
   def index
-    @races = Race.all(:conditions => {:season => season.to_i})
+    @races = Race.all(:conditions => {:season => season.to_i}, :order => :date)
   end
 end
