244307b7320e42c40cd714308fbb121eb21f15da
Heinrich Klobuczek <heinrich@mail.com>
2010-09-22 21:44:26 -0300

    minor view cleanup

---
+++ app/helpers/result_helper.rb
@@ -0,0 +1,9 @@
+module ResultHelper
+  def details result
+    "#{result.race.place} - #{result.race.discipline}#{", #{result.cup_points} pts, #{result.fis_points} FIS points" if result.rank}"
+  end
+
+  def considered result_counter, result
+    result_counter < season.max_races and result.successful?
+  end
+end
