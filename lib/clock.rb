3e1493856dca7176a83593db418de33a8e6aa87e
Heinrich Klobuczek <heinrich@mail.com>
2014-12-30 17:28:38 +0100

    fixed bug with current Season

--- lib/clock.rb
+++ lib/clock.rb
@@ -5,7 +5,7 @@ require_relative '../config/environment'
 module Clockwork
   handler do |job|
     puts "Running #{job}"
-    ActiveRecord::Base.transaction { FisParser.load_season Season.current }
+    ActiveRecord::Base.transaction { FisParser.load_season Season.current.to_i }
   end
 
   every(1.hour, 'load')
